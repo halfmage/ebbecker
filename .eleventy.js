33546997e706e030b36ea8b0947731017b32dbca
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-02-07 16:46:46 +0100

    initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
 const htmlmin = require("html-minifier");
+const Image = require("@11ty/eleventy-img");
+const sharp = require("sharp");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("fonts");
+
+  // Custom files
+  eleventyConfig.addPassthroughCopy("./JobFoodZertifikat.pdf");
+  eleventyConfig.addPassthroughCopy("./site.webmanifest");
+  eleventyConfig.addPassthroughCopy("./social.png");
 
   eleventyConfig.addWatchTarget("./_tmp/style.css");
 
@@ -14,6 +23,58 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "Image",
+    async (src, alt, classImage, classContainer) => {
+      if (!alt) {
+        throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+      }
+
+      let stats = await Image(src, {
+        widths: [25, 300, 600, 900, 1200, 2400],
+        formats: ["jpg", "webp"],
+        urlPath: "/images/",
+        outputDir: "./_site/images/",
+      });
+
+      let lowestSrc = stats["jpg"][0];
+
+      const placeholder = await sharp(lowestSrc.outputPath)
+        .resize({ fit: sharp.fit.inside })
+        .blur()
+        .toBuffer();
+
+      const base64Placeholder = `data:image/png;base64,${placeholder.toString(
+        "base64"
+      )}`;
+
+      const srcset = Object.keys(stats).reduce(
+        (acc, format) => ({
+          ...acc,
+          [format]: stats[format].reduce(
+            (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+            ""
+          ),
+        }),
+        {}
+      );
+
+      const source = `<source type="image/webp" data-srcset="${srcset["webp"]}" >`;
+
+      const img = `<img
+      class="lazy ${classImage}"
+      alt="${alt}"
+      src="${base64Placeholder}"
+      data-src="${lowestSrc.url}"
+      data-sizes='(min-width: 1024px) 1024px, 100vw'
+      data-srcset="${srcset["jpeg"]}"
+      width="${lowestSrc.width}"
+      height="${lowestSrc.height}">`;
+
+      return `<picture class="${classContainer}">${source} ${img}</picture>`;
+    }
+  );
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (
--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,4 +1,8 @@
+const { white } = require("tailwindcss/colors");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  darkMode: "media",
   purge: {
     content: ["_site/**/*.html"],
     options: {
@@ -6,12 +10,26 @@ module.exports = {
     },
   },
   theme: {
-    extend: {
-      colors: {
-        change: "black",
-      },
+    fontFamily: {
+      sans: ["Work Sans", "sans-sans"],
+      serif: ["Kalam", "sans-sans"],
+    },
+    colors: {
+      gray: colors.warmGray,
+      primary: colors.lime,
+      white: white,
+      beratung: "#64A30C",
+      "beratung-light": "#C4EF87",
+      therapie: "#8B5CF6",
+      "therapie-light": "#DDD6FF",
+      bia: "#F49E0B",
+      "bia-light": "#FDE689",
+      bgf: "#2563EA",
+      "bgf-light": "#DBEAFF",
+      covid: colors.red[500],
+      "covid-light": colors.red[200],
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
